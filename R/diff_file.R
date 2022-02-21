2fec275cbf94b8b1c8b5b89865209177ad31e8fa
Arni Magnusson <thisisarni@gmail.com>
2022-02-21 18:02:04 +1100

    Add diff_file()

---
+++ R/diff_file.R
@@ -0,0 +1,52 @@
+#' Compare R File
+#'
+#' Compare the source code of a given R file line by line, between the \pkg{TAF}
+#' and \pkg{icesTAF} repositories.
+#'
+#' @param file name of a \file{*.R} file, without the full path.
+#' @param tools.prod location of \code{TAF} and \code{icesTAF} repositories.
+#' @param ignore.overview whether to ignore lines containing the string
+#'        \code{"gives an overview of the package"}.
+#' @param simplify whether to omit empty string vectors in the output.
+#'
+#' @return
+#' List containing lines of code that are different between the R file in the
+#' two repositories.
+#'
+#' @examples
+#' \dontrun{
+#' tools.prod <- "~/git/ices/tools-prod"
+#' diff_file("draft.data.R", tools.prod)
+#' }
+#'
+#' @export
+
+diff_file <- function(file, tools.prod="~/git/ices/tools-prod",
+                      ignore.overview=TRUE, simplify=TRUE)
+{
+  TAF <- readLines(file.path(tools.prod, "TAF/R", file))
+  icesTAF <- readLines(file.path(tools.prod, "icesTAF/R", file))
+  if(ignore.overview)
+  {
+    omit <- "gives an overview of the package"
+    TAF <- grep(omit, TAF, invert=TRUE, value=TRUE)
+    icesTAF <- grep(omit, icesTAF, invert=TRUE, value=TRUE)
+  }
+
+  diff.TAF <- setdiff(TAF, icesTAF)
+  diff.icesTAF <- setdiff(icesTAF, TAF)
+
+  out <- list(TAF=diff.TAF, icesTAF=diff.icesTAF)
+
+  if(simplify)
+  {
+    if(length(out$TAF) == 0)
+      out$TAF <- NULL
+    if(length(out$icesTAF) == 0)
+      out$icesTAF <- NULL
+    if(length(out) == 0)
+      out <- NULL
+  }
+
+  out
+}
